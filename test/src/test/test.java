eff3d4fde1646859cc74f1d838a54d9d495c383b
694724257 <694724257@qq.com>
2019-05-19 15:13:12 +0800

    44

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-/////
+////////
 public class test extends JFrame {
 //定义组件
 	public static void main(String[] args) {
